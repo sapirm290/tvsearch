0cd1e8c593407d9a41560d3d2a412eeb1651f12e
Sapir Malka <sapirm290@gmail.com>
2019-07-15 17:01:52 +0300

    Revert "Main url handers"
    This reverts commit 2dbcae05ad1c99f777c2733b28ee18712ac7439f.

--- main.py
+++ main.py
@@ -5,59 +5,21 @@ import utils
 
 # Static Routes
 
-
 @get("/js/<filepath:re:.*\.js>")
 def js(filepath):
     return static_file(filepath, root="./js")
-
 
 @get("/css/<filepath:re:.*\.css>")
 def css(filepath):
     return static_file(filepath, root="./css")
 
-
 @get("/images/<filepath:re:.*\.(jpg|png|gif|ico|svg)>")
 def img(filepath):
     return static_file(filepath, root="./images")
 
-# dynamic routes
-
-
 @route('/')
 def index():
     sectionTemplate = "./templates/home.tpl"
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData={})
+    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData = {})
 
-
-@route('/browse')
-def browse():
-    sectionTemplate = "./templates/browse.tpl"
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData={})
-
-
-@route('/show')
-def show():
-    sectionTemplate = "./templates/show.tpl"
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData={})
-
-
-@route('/episode')
-def episode():
-    sectionTemplate = "./templates/episode.tpl"
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData={})
-
-
-@route('/search')
-def search():
-    sectionTemplate = "./templates/search.tpl"
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData={})
-
-
-@route('/search_result')
-def search_result():
-    sectionTemplate = "./templates/search_result.tpl"
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData={})
-
-
-
-run(host='0.0.0.0', port=os.environ.get('PORT', 5000), reloader=True)
+run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
