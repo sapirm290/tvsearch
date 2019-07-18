16252e8bb4cac6f5dc8dfdf4f3be63908b56dd90
Sapir Malka <sapirm290@gmail.com>
2019-07-18 14:36:05 +0300

    Added get_show function at utils, detailed imports at main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from bottle import (get, post, redirect, request, route, run, static_file,
-                    template, redirect)
-import utils
+                    template, redirect, error)
+from utils import getVersion, getJsonFromFile, find_ep, find_show, AVAILABE_SHOWS
 import json
 # Static Routes
 
@@ -26,52 +26,56 @@ def img(filepath):
 @route('/')
 def index():
     sectionTemplate = "./templates/home.tpl"
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData={})
+    return template("./pages/index.html", version=getVersion(), sectionTemplate=sectionTemplate, sectionData={})
 
 
 @route('/browse')
 def browse():
-    sectionData = [json.loads(utils.getJsonFromFile(show))
-                   for show in utils.AVAILABE_SHOWS]
+    sectionData = [json.loads(getJsonFromFile(show))
+                   for show in AVAILABE_SHOWS]
     sectionTemplate = "./templates/browse.tpl"
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData=sectionData)
+    return template("./pages/index.html", version=getVersion(), sectionTemplate=sectionTemplate, sectionData=sectionData)
 
 
 @route('/show/<show_id:int>/episode/<ep_id:int>')
 def episode(show_id, ep_id):
     sectionTemplate = "./templates/episode.tpl"
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData=utils.find_ep(show_id, ep_id))
+    return template("./pages/index.html", version=getVersion(), sectionTemplate=sectionTemplate, sectionData=find_ep(show_id, ep_id))
 
 
 @route('/ajax/show/<show_id>/episode/<ep_id:int>')
 def episode_request(show_id, ep_id):
-    return template("./templates/episode.tpl",  result=utils.find_ep(show_id, ep_id))
+    return template("./templates/episode.tpl",  result=find_ep(show_id, ep_id))
 
 
 @route('/show/<show_id:int>')
 def show(show_id):
-    sectionData = json.loads(utils.getJsonFromFile(show_id))
     sectionTemplate = "./templates/show.tpl"
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData=sectionData)
+    return template("./pages/index.html", version=getVersion(), sectionTemplate=sectionTemplate, sectionData=find_show(show_id))
 
 
 @route('/ajax/show/<show_id>')
 def show_request(show_id):
-    sectionData = json.loads(utils.getJsonFromFile(show_id))
-    return template("./templates/show.tpl",  result=sectionData)
+    return template("./templates/show.tpl",  result=find_show(show_id))
 
 
 @route('/search')
 def search():
     sectionTemplate = "./templates/search.tpl"
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData={})
+    return template("./pages/index.html", version=getVersion(), sectionTemplate=sectionTemplate, sectionData={})
 
 
 @post('/search')
 def search_result():
     sectionTemplate = "./templates/search_result.tpl"
     query = request.forms.get('q')
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData={}, results={}, query=query)
+    return template("./pages/index.html", version=getVersion(), sectionTemplate=sectionTemplate, sectionData={}, results={}, query=query)
+
+
+@error(404)
+def error404(error):
+    sectionTemplate = "./templates/404.tpl"
+    return template("./pages/index.html", version=getVersion(), sectionTemplate=sectionTemplate, sectionData={})
 
 
 run(host='localhost', port=os.environ.get(
--- utils.py
+++ utils.py
@@ -2,18 +2,27 @@ from bottle import template
 import json
 
 JSON_FOLDER = './data'
-AVAILABE_SHOWS = ["7", "66", "73", "82", "112", "143", "175", "216", "1371", "1871","2993", "305"]
+AVAILABE_SHOWS = ["7", "66", "73", "82", "112", "143",
+                  "175", "216", "1371", "1871", "2993", "305"]
+
 
 def getVersion():
     return "0.0.1"
+
 
 def getJsonFromFile(showName):
     try:
         return template("{folder}/{filename}.json".format(folder=JSON_FOLDER, filename=showName))
     except:
         return "{}"
+
+
 def find_ep(show_id, ep_id):
     for ep in json.loads(getJsonFromFile(show_id))['_embedded']['episodes']:
         if ep['id'] == ep_id:
             return ep
     return None
+
+
+def find_show(show_id):
+    return json.loads(getJsonFromFile(show_id))
